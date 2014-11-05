eb8f4cdbc3c8deeadda8508b33bdb44f2638584d
Angel Carballo <angelcarballo@gmail.com>
2014-11-05 17:23:16 +0100

    Guard and rspec adjustments

--- Guardfile
+++ Guardfile
@@ -1,11 +1,5 @@
-guard 'rspec', cmd: 'bundle exec rspec' do
-  # watch /lib/ files
-  watch(%r{^lib/(.+).rb$}) do |m|
-    "spec/#{m[1]}_spec.rb"
-  end
-
-  # watch /spec/ files
-  watch(%r{^spec/(.+).rb$}) do |m|
-    "spec/#{m[1]}.rb"
-  end
+guard 'rspec', all_on_start: false, cmd: 'bundle exec rspec' do
+  watch(/^spec\/.+_spec\.rb/)
+  watch(/^lib\/(.+)\.rb/)     { |m| "spec/#{m[1]}_spec.rb"  }
+  watch('spec/spec_helper.rb') { 'spec'  }
 end
--- lib/asm/collection.rb
+++ lib/asm/collection.rb
@@ -3,14 +3,14 @@ require 'erb'
 class Asm::Collection
   DEFAULT_ENDPOINT = 'http://www.asmred.com/WebSrvs/b2b.asmx?op=GrabaServicios'
   TEMPLATE = File.expand_path("../../../templates/collection.xml.erb", __FILE__)
-  ALLOWED_OPTIONS = [:client_uid, :date, :time_from, :time_to, :reference_number, :name,
+  OPTIONS = [:client_uid, :date, :time_from, :time_to, :reference_number, :name,
                      :address, :city, :state, :country_code, :zipcode, :phone, :mobile, :email]
 
-  attr_accessor *ALLOWED_OPTIONS
+  attr_accessor *OPTIONS
 
   def initialize(options = {})
-    ALLOWED_OPTIONS.each do |option|
-      instance_variable_set "@#{option}", options[option]
+    OPTIONS.each do |op|
+      instance_variable_set "@#{op}", options[op]
     end
   end
 
