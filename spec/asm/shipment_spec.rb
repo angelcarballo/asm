8d6d7b7e8abd2829b56d6983175306a1da669d95
Angel Carballo <angelcarballo@gmail.com>
2014-11-10 18:27:49 +0100

    Test cleanup

--- spec/asm/shipment_spec.rb
+++ spec/asm/shipment_spec.rb
@@ -1,14 +1,14 @@
-require 'Asm'
+require 'byebug'
+require 'asm'
 require 'yaml'
 
 describe Asm::Shipment do
 
   it 'generates a valid template' do
-    valid_options = YAML.load_file(File.expand_path('../../support/valid_shipment.xml', __FILE__))
+    valid_options = YAML.load_file(File.expand_path('../../support/valid_shipment_options.yml', __FILE__))
     valid_message = File.read(
       File.expand_path('../../support/valid_shipment.xml', __FILE__)
     ).lines.map(&:strip).join
-    debugger
 
     collection = Asm::Shipment.new(valid_options)
 
